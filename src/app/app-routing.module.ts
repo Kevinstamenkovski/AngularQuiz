38585ab5b461b88f29aba14ff6a5714885f07714
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-03-12 15:48:18 +0100

    Creating Files (css not finished)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FifthQuestionComponent } from './fifth-question/fifth-question.component';
+import { FirstQuestionComponent } from './first-question/first-question.component';
+import { FourthQuestionComponent } from './fourth-question/fourth-question.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RestartPageComponent } from './restart-page/restart-page.component';
+import { SecondQuestionComponent } from './second-question/second-question.component';
+import { ThirdQuestionComponent } from './third-question/third-question.component';
+import { WinPageComponent } from './win-page/win-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main-page', component: MainPageComponent},
+  { path: 'restart-page', component: RestartPageComponent},
+  { path: 'first-question', component: FirstQuestionComponent},
+  { path: 'second-question', component: SecondQuestionComponent},
+  { path: 'third-question', component: ThirdQuestionComponent},
+  { path: 'third-question', component: ThirdQuestionComponent},
+  { path: 'fourth-question', component: FourthQuestionComponent},
+  { path: 'fifth-question', component: FifthQuestionComponent},
+  { path: 'win-page', component: WinPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  MainPageComponent, 
+  FirstQuestionComponent, 
+  SecondQuestionComponent, 
+  ThirdQuestionComponent, 
+  FourthQuestionComponent, 
+  FifthQuestionComponent, 
+  WinPageComponent,
+  RestartPageComponent
+];
